4ed206e7045120a80eda2cfb239172cdbb4cef70
Vinu Sebastian <winusebastian@users.noreply.github.com>
2017-01-18 16:54:50 +0400

    Update WModel.php

--- WBase/framework/core/WModel.php
+++ WBase/framework/core/WModel.php
@@ -1,17 +1,23 @@
 <?php
 
-class WModel{
-       
-    public $_db;
-    public function __construct() {
-        $this->_db = WB::app()->db;
+class WModel {
+    
+    private $db;
+    
+    public function __construct(){
+        $this->db = WB::app()->db;
     }
 
-    public function CreateQuery($sql = '') {
+    public function RunQuery($sql = '') {
+       
         if (!empty($sql)) {
-            $result = mysqli_query($this->_db, $sql);
-            return $result;
-        }  else {
+            if (isset($this->db)) {
+                $result = mysqli_query($this->db, $sql);
+                return $result;
+            } else {
+                throw new WException('Define a db in config.');
+            }
+        } else {
             throw new WException('Empty string is given.');
         }
     }
