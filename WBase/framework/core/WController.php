c271c1291473567a95f472024a176884204f02e0
Vinu Sebastian <winusebastian@users.noreply.github.com>
2017-01-18 16:54:17 +0400

    Update WController.php

--- WBase/framework/core/WController.php
+++ WBase/framework/core/WController.php
@@ -1,17 +1,28 @@
 <?php
 
-class WController extends WComponent{
+class WController extends WComponent {
 
-    public function init() {
+    private function LoadTheme() {
+        if (isset(WB::app()->config['theme']) && !empty(WB::app()->config['theme'])) {
+            WB::$layout = isset(WB::$layout) ? WB::$layout : 'main';
+            if (file_exists(WB_APPLICATION . '/themes/' . WB::app()->config['theme'] . '/layouts/' . WB::$layout . '.php')) {
+                return WB_APPLICATION . '/themes/' . WB::app()->config['theme'] . '/layouts/' . WB::$layout . '.php';
+            } else {
+                throw new WException('Invalid layout or theme given.');
+            }
+        } else {
+            throw new WException('Invalid theme given.');
+        }
     }
 
     public function render($file = '', $data = array()) {
+
         $reflector = new ReflectionClass(get_called_class());
         $fn = $reflector->getFileName();
         $filename = dirname($fn) . '/../view/' . get_called_class() . '/' . $file . '.php';
         if (file_exists($filename)) {
             $content = $this->renderToString($filename, $data);
-            $html = $this->renderToString(WB::app()->LoadTheme(), array('content' => $content));
+            $html = $this->renderToString($this->LoadTheme(), array('content' => $content));
             echo $html;
         } else {
             throw new WException('Invalid view file "' . $file . '" of controller "' . get_called_class() . '".');
@@ -22,6 +33,7 @@ class WController extends WComponent{
         if (is_array($vars) && !empty($vars)) {
             extract($vars);
         }
+        $themeurl = WB::app()->themeurl;
         ob_start();
         include $file;
         return ob_get_clean();
