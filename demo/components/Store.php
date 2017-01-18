4e55f2c280b9fceaa59c0525effcdcbe9503ccc0
Vinu Sebastian <winusebastian@users.noreply.github.com>
2017-01-18 16:59:40 +0400

    Update Store.php

--- demo/components/Store.php
+++ demo/components/Store.php
@@ -2,8 +2,6 @@
 
 class Store extends WContainer {
 
-    public $data = array();
-
     public function __construct() {
         $services = array(
             'MOD_CLASS' => 'Data'
@@ -13,18 +11,4 @@ class Store extends WContainer {
         }
     }
 
-    public function __set($key, $value) {
-        $this->data[$key] = $value;
-    }
-
-    public function __get($key) {
-        return isset($this->data[$key]) ? $this->data[$key] : NULL;
-    }
-
-    public function __unset($key) {
-        unset($this->data[$key]);
-    }
-
 }
-
-?>
